0d0f66d05454aff33b005894b7b48c01471b9230
Paul H <161055444+flopsllc@users.noreply.github.com>
2025-05-26 14:26:44 -0500

    no spinning

--- classes/updaters/github-com.php
+++ classes/updaters/github-com.php
@@ -2,13 +2,14 @@
 
 namespace ReallySpecific\WP_Util\Updaters\GitHub;
 
-add_filter( 'rs_util_updater_theme_update_uri_github.com', __NAMESPACE__ . '\\filter_update_uri', 10, 2 );
-add_filter( 'rs_util_updater_plugin_update_uri_github.com', __NAMESPACE__ . '\\filter_update_uri', 10, 2 );
+add_filter( 'rs_util_updater_theme_update_uri_github.com', __NAMESPACE__ . '\\filter_update_uri', 10, 1 );
+add_filter( 'rs_util_updater_plugin_update_uri_github.com', __NAMESPACE__ . '\\filter_update_uri', 10, 1 );
 add_filter( 'rs_util_updater_package_retrieval_uri_github.com', __NAMESPACE__ . '\\filter_package_retrieval_uri', 10, 2 );
 add_filter( 'rs_util_updater_package_body_github.com', __NAMESPACE__ . '\\filter_package_body', 10, 2 );
 
-function filter_update_uri( $uri, $parsed_repo ) {
-	return "https://api.github.com/repos{$parsed_repo['path']}";
+function filter_update_uri( $uri ) {
+	$path = parse_url( $uri, PHP_URL_PATH );
+	return "https://api.github.com/repos{$path}";
 }
 
 function filter_package_retrieval_uri( $uri ) {
