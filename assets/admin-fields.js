989ba94d902cc0a57f213d6b9dffbf6eb6a8cc07
Paul H <161055444+flopsllc@users.noreply.github.com>
2025-06-24 10:29:28 -0400

    tom!

--- assets/admin-fields.js
+++ assets/admin-fields.js
@@ -1,6 +1,6 @@
 ( async function() {
 
-	const TomSelect = require( 'tom-select/popular' );
+	const TomSelect = await import( 'tom-select/popular' );
 
 	const form = document.querySelector( '.rs-util-settings-form' );
 	if ( ! form ) {
