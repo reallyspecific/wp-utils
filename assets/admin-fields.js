5493ea6f1ff5ca811f6a05298bef558699875913
Paul H <161055444+flopsllc@users.noreply.github.com>
2025-06-24 10:56:35 -0400

    tom are you there

--- assets/admin-fields.js
+++ assets/admin-fields.js
@@ -1,43 +1,83 @@
-( async function() {
+import TomSelect from 'tom-select/popular';
 
-	const TomSelect = await import( 'tom-select/popular' );
+const thisForm = () => {
+	return document.querySelector( '.rs-util-settings-form' );
+}
 
-	const form = document.querySelector( '.rs-util-settings-form' );
-	if ( ! form ) {
-		return;
-	}
+const initForm = () => {
 
-	const onReady = async () => {
-
-		const settings = rsUtil_settingsPageENV();
-
-		if ( settings.svg_iconset && ! document.querySelector( '#rs-util-svg-iconset' ) ) {
-			const svgIcons = document.createElement( 'div' );
-			try {
-				const svg = await fetch( settings.svg_iconset );
-				const svgText = await svg.text();
-				svgIcons.innerHTML = svgText;
-			} catch ( e ) {
-				console.error( e );
-			}
-			document.body.appendChild( svgIcons.firstElementChild );
+	const allToggledBy = thisForm().querySelectorAll( '[data-toggled-by]' );
+	allToggledBy.forEach( toggleTarget => {
+		const toggle = thisForm().querySelector( `#${toggleTarget.dataset.toggledBy}` );
+		if ( ! toggle ) {
+			return;
 		}
+		showTogglableControls( toggle );
+	} );
 
-		document.querySelectorAll( '[data-use-tom-select]' ).forEach( el => {
-			el.tom = new TomSelect( el );
-		} );
+}
 
+const onReady = async () => {
+
+	const settings = rsUtil_settingsPageENV();
+
+	if ( settings.svg_iconset && ! document.querySelector( '#rs-util-svg-iconset' ) ) {
+		const svgIcons = document.createElement( 'div' );
+		try {
+			const svg = await fetch( settings.svg_iconset );
+			const svgText = await svg.text();
+			svgIcons.innerHTML = svgText;
+		} catch ( e ) {
+			console.error( e );
+		}
+		document.body.appendChild( svgIcons.firstElementChild );
 	}
 
-	if ( typeof rsUtil_settingsPageENV === 'function' ) {
-		onReady();
-	} else {
-		document.addEventListener( 'rsUtil_settingsPageENV|ready', onReady );
+	document.querySelectorAll( '[data-use-tom-select]' ).forEach( el => {
+		el.tom = new TomSelect( el, {} );
+	} );
+
+}
+
+const switchTab = ( section ) => {
+
+	if ( section === '' ) {
+		const firstSection = thisForm().querySelector( `.rs-util-settings-section[data-section]` );
+		section = firstSection.dataset.section;
 	}
 
-	function enableSaveButton() {
-		document.querySelector( '.rs-util-settings-page .rs-util-settings-page__submit' ).disabled = false;
+	const tabToggles = document.querySelectorAll( '.rs-util-settings-page__tab-toggle' );
+	tabToggles.forEach( toggle => {
+		toggle.setAttribute( 'aria-expanded', toggle.dataset.section === section ? 'true' : 'false' );
+		const target = thisForm().querySelector( `.rs-util-settings-section[data-section="${toggle.dataset.section}"]` );
+		target.setAttribute( 'aria-hidden', toggle.dataset.section === section ? 'false' : 'true' );
+	} );
+}
+
+
+const showTogglableControls = ( toggle ) => {
+	const toggled = thisForm().querySelectorAll( `[data-toggled-by="${toggle.id}"]` );
+	toggled.forEach( fieldRow => {
+		fieldRow.setAttribute( 'aria-hidden', ( toggle.checked ?? ( toggle.value || false ) ) ? 'false' : 'true' );
+	} );
+	if ( toggled.length ) {
+		toggle.setAttribute( 'aria-expanded', ( toggle.checked ?? ( toggle.value || false ) ) ? 'true' : 'false' );
+		toggle.setAttribute( 'aria-controls', `[data-toggled-by="${toggle.id}"]` );
+		toggle.setAttribute( 'data-controls', `[data-toggled-by="${toggle.id}"]` );
 	}
+}
+
+const onPopState = () => {
+	const url = new URL( window.location.href );
+	const section = url.hash.replace( '#', '' );
+	switchTab( section );
+}
+
+const enableSaveButton = () => {
+	document.querySelector( '.rs-util-settings-page .rs-util-settings-page__submit' ).disabled = false;
+}
+
+const initListeners = () => {
 
 	document.addEventListener( 'change', e => {
 		if ( e.target.closest( '.rs-util-settings-form' ) ) {
@@ -51,8 +91,8 @@
 			return;
 		}
 		e.preventDefault();
-		form.classList.add('is-state-saving');
-		form.submit();
+		thisForm().classList.add('is-state-saving');
+		thisForm().submit();
 	} );
 
 	document.addEventListener( 'click', e => {
@@ -66,21 +106,6 @@
 		switchTab( tabToggle.dataset.section );
 	} );
 
-	const switchTab = ( section ) => {
-
-		if ( section === '' ) {
-			const firstSection = form.querySelector( `.rs-util-settings-section[data-section]` );
-			section = firstSection.dataset.section;
-		}
-
-		const tabToggles = document.querySelectorAll( '.rs-util-settings-page__tab-toggle' );
-		tabToggles.forEach( toggle => {
-			toggle.setAttribute( 'aria-expanded', toggle.dataset.section === section ? 'true' : 'false' );
-			const target = form.querySelector( `.rs-util-settings-section[data-section="${toggle.dataset.section}"]` );
-			target.setAttribute( 'aria-hidden', toggle.dataset.section === section ? 'false' : 'true' );
-		} );
-	}
-
 	document.addEventListener( 'change', e => {
 		const toggle = e.target.closest( '[data-controls]' );
 		if ( ! toggle ) {
@@ -91,35 +116,15 @@
 		showTogglableControls( toggle );
 	} );
 
-	const showTogglableControls = ( toggle ) => {
-		const toggled = form.querySelectorAll( `[data-toggled-by="${toggle.id}"]` );
-		toggled.forEach( fieldRow => {
-			fieldRow.setAttribute( 'aria-hidden', ( toggle.checked ?? ( toggle.value || false ) ) ? 'false' : 'true' );
-		} );
-		if ( toggled.length ) {
-			toggle.setAttribute( 'aria-expanded', ( toggle.checked ?? ( toggle.value || false ) ) ? 'true' : 'false' );
-			toggle.setAttribute( 'aria-controls', `[data-toggled-by="${toggle.id}"]` );
-			toggle.setAttribute( 'data-controls', `[data-toggled-by="${toggle.id}"]` );
-		}
-	}
-
-	const allToggledBy = form.querySelectorAll( '[data-toggled-by]' );
-	allToggledBy.forEach( toggleTarget => {
-		const toggle = form.querySelector( `#${toggleTarget.dataset.toggledBy}` );
-		if ( ! toggle ) {
-			return;
-		}
-		showTogglableControls( toggle );
-	} );
-
-	const onPopState = () => {
-		const url = new URL( window.location.href );
-		const section = url.hash.replace( '#', '' );
-		switchTab( section );
-	}
-
 	window.addEventListener( 'popstate', onPopState );
+}
 
-	onPopState();
+onPopState();
+initListeners();
+initForm();
 
-} )();
+if ( typeof rsUtil_settingsPageENV === 'function' ) {
+	onReady();
+} else {
+	document.addEventListener( 'rsUtil_settingsPageENV|ready', onReady );
+}
