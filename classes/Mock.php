49fc471c23c633920763b6a54373f7f89d633ec7
Paul H <161055444+flopsllc@users.noreply.github.com>
2025-07-28 14:31:57 -0400

    more composer updates

--- classes/Mock.php
+++ classes/Mock.php
@@ -7,12 +7,21 @@ use ReallySpecific\Utils\Mocks\Functions;
 class Mock extends \WP_Mock {
 
 	static Functions $functions;
+
+	public static function bootstrap(): void
+	{
+		parent::bootstrap();
+
+	}
+
 	public static function setUp(): void {
 		parent::setUp();
 		if ( empty( static::$functions ) ) {
 			static::$functions = new Functions();
 		}
 	}
+
+
 
 	public static function didAction( $action ) {
 		return static::$event_manager->called( $action );
