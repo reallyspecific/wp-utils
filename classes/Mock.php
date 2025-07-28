8cc2b413ea07f48f312873e10e1d0732e38a7550
Paul H <161055444+flopsllc@users.noreply.github.com>
2025-07-28 14:36:38 -0400

    updates to Mock class

--- classes/Mock.php
+++ classes/Mock.php
@@ -1,23 +1,46 @@
 <?php
 
+/**
+ * Provides mocking utilities for usage in testing environments,
+ * extending the functionality of WP_Mock and supporting additional customization.
+ */
+
 namespace ReallySpecific\Utils;
 
 use ReallySpecific\Utils\Mocks\Functions;
 
+/**
+ * Extends the functionality of the WP_Mock class to include additional mock capabilities.
+ * Provides methods to bootstrap and set up mock functions and register actions.
+ */
 class Mock extends \WP_Mock {
 
-	static Functions $functions;
+	/**
+	 *  Extended function instance
+	 *
+	 *  @var Functions
+	 */
+	static Functions $more_functions;
 
-	public static function bootstrap(): void
-	{
+	/**
+	 * Initializes the application by calling the parent bootstrap method
+	 * and performing additional setup specific to this class.
+	 *
+	 * @return void
+	 */
+	public static function bootstrap(): void {
 		parent::bootstrap();
-
+		static::setUp();
 	}
 
+	/**
+	 * Initializes and configures necessary static properties.
+	 *
+	 * @return void
+	 */
 	public static function setUp(): void {
-		parent::setUp();
-		if ( empty( static::$functions ) ) {
-			static::$functions = new Functions();
+		if ( empty( static::$more_functions ) ) {
+			static::$more_functions = new Functions();
 		}
 	}
 
@@ -26,5 +49,4 @@ class Mock extends \WP_Mock {
 	public static function didAction( $action ) {
 		return static::$event_manager->called( $action );
 	}
-
 }
